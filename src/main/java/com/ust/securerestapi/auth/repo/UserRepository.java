1703a7dd814fcbc4821fa5c6e51d7e28615162f8
Ashish.s.panicker <ashish.s.panicker@outlook.com>
2024-08-28 10:04:07 +0530

    Created User repo.

---
+++ src/main/java/com/ust/securerestapi/auth/repo/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ust.securerestapi.auth.repo;
+
+import com.ust.securerestapi.auth.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserName(String userName);
+}
